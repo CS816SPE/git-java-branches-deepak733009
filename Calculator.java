fb420afd5993b6ea370ebc268a0e474c205a335d
deepak733009 <deepak.devarakonda96@gmail.com>
2019-01-17 11:46:53 +0530

    Added Multiplication Module

--- Calculator.java
+++ Calculator.java
@@ -15,7 +15,9 @@ public class Calculator
         {
             System.out.print("1. Addition\n");
             System.out.print("2. Subtraction\n");
-            System.out.print("3. Exit\n\n");
+            System.out.print("3. Multiplication\n\n");
+	    System.out.print("4. Division\n\n");
+	    System.out.print("5. Exit\n\n");
             System.out.print("Enter Your Choice : ");
             choice = scan.next().charAt(0);
             switch(choice)
@@ -32,7 +34,13 @@ public class Calculator
                     res = a - b;
                     System.out.print("Result = " + res);
                     break;
-                case '3' : System.exit(0);
+		case '3': System.out.print("Enter Two NUmber:");
+			  a=scan.nextFloat();
+			  b=scan.nextFloat();
+			  res =a * b;
+			  System.out.print("Result ="+res);
+			  break;
+                case '5' : System.exit(0);
                     break;
                 default : System.out.print("Wrong Choice!!!");
                     break;
